78760b66a52de264e8ea52721022841504b49c06
bee-squared <b.bouch@gmail.com>
2024-06-09 21:32:59 -0600

    Handle authentication and login

--- website/views.py
+++ website/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-  return render(request, 'home.html', {})
+  # Check to see if logging in
+  if request.method == 'POST':
+    print(request)
+    username = request.POST['username'] # matches the form field name username
+    password = request.POST['password'] # matches the form field name password
+
+    #Authenticate
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      messages.success(request, "You have been logged in!")
+      return redirect('home')
+    else:
+      messages.error(request, "There was an error logging in, please try again.")
+      return redirect('home')
+  else:
+    return render(request, 'home.html', {})
+
+def login_user(request):
+  pass
+
+def logout_user(request):
+  pass
+
