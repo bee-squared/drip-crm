503de1a315f5e76ae9cce65a300ca054de1534a6
bee-squared <b.bouch@gmail.com>
2024-06-11 12:38:51 -0600

    Complete add record form and include cancel button to bring user to home page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
+
 
 def home(request):
   records = Record.objects.all()
@@ -67,3 +68,18 @@ def delete_record(request, pk):
     return redirect('home')
   else:
     messages.success(request, "You must be logged in to delete records!")
+
+def add_record(request):
+  form = AddRecordForm(request.POST or None)
+  if request.user.is_authenticated:
+    if request.method == "POST":
+      if form.is_valid():
+        add_record = form.save()
+        messages.success(request, "Record Added....")
+        return redirect('home')
+    return render(request, 'add_record.html', {
+      'form':form
+    })
+  else:
+    messages.success(request, "You must be logged in to add new records.")
+    return redirect('home')
