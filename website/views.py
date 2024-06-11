c2bd2521dc1ffd62d6b7d9d346cb74cf6c83236b
bee-squared <b.bouch@gmail.com>
2024-06-11 10:36:18 -0600

    Add link to empty customer record

--- website/views.py
+++ website/views.py
@@ -49,3 +49,12 @@ def register_user(request):
     return render(request, 'register.html', {'form':form})
 
   return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk): # e.g. localhost:8000/record/3213
+  if request.user.is_authenticated:
+    # look up record
+    customer_record = Record.objects.get(id=pk)
+    return render(request, 'record.html', {'customer_record': customer_record})
+  else:
+    messages.success(request, "You must be logged in to view customers")
+    return redirect('home')
